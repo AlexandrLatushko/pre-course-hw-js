4267079647d614104684feb3891437f9866372ea
AlexandrLatushko <latushkoalex94@gmail.com>
2024-03-28 16:27:37 +0300

    add 04.js

---
+++ lesson-01/04.js
@@ -0,0 +1,22 @@
+// 4 задание
+
+// Посчитай и выведи окончательный результат в console.log.
+// let x = 5;
+// x += 3;
+// x *= 2;
+// x -= 4;
+// x /= 3;
+
+
+let x = 5;
+x += 3;
+console.log(x);
+
+x *= 2;
+console.log(x);
+
+x -= 4;
+console.log(x);
+
+x /= 3;
+console.log(x);
