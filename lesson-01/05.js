7cd3272ed7e062ad9d9df8a68d2e9eb289d1791e
AlexandrLatushko <latushkoalex94@gmail.com>
2024-04-03 13:48:18 +0300

    5 js

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -8,9 +8,6 @@ let rule =
   'Еще не родился тот человек, который поставил бы цель и не смог бы стать программистом.';
 
 switch (true) {
-  case rule.length === 25:
-    console.log('50 на 50');
-    break; 
   case rule.length < 25:
     console.log('Все таки нет правил без исключения');
     break;
@@ -20,7 +17,7 @@ switch (true) {
   break
  
   default:
-    console.log('Другое условие');
+    console.log('50 на 50');
     break;
  
   }
