2a12f2f6dc2ea49a79d12dbe51366dc7a9503207
AlexandrLatushko <latushkoalex94@gmail.com>
2024-04-10 13:06:25 +0300

    add 07.js lesson 2

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -1,13 +1,30 @@
-let passportMarried = {
-    name: "Petr",
-    surname: "Petrov",
-    address: {
-        country: "USA",
-        city: "Bobryisk"
+function deepClone(obj) {
+    if (obj === null || typeof obj !== 'object') {
+        return obj;
     }
-};
-
-let passportMarried2 = { ...passportMarried, married: true };
-
-console.log(passportMarried);
-console.log(passportMarried2);
+ 
+    const clone = Array.isArray(obj) ? [] : {};
+ 
+    for (let key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            clone[key] = deepClone(obj[key]);
+        }
+    }
+ 
+    return clone;
+ }
+ 
+ let passportMarried = {
+     name: "Petr",
+     surname: "Petrov",
+     address: {
+         country: "USA",
+         city: "Bobryisk"
+     }
+ };
+ 
+ let passportMarried2 = deepClone(passportMarried);
+ passportMarried2.married = true;
+ 
+ console.log(passportMarried);
+ console.log(passportMarried2);
