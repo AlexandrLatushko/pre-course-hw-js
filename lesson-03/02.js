7a960e1794fa74a4f5fc14e002b2d875b7c44377
AlexandrLatushko <latushkoalex94@gmail.com>
2024-04-15 17:36:35 +0300

    add 02.js lesson3

---
+++ lesson-03/02.js
@@ -0,0 +1,10 @@
+
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"]; 
+
+const newWoeds = words.indexOf("апельсин")
+
+if(newWoeds === -1){
+    console.log('«Придется поискать в другом магазине…»')
+} else {
+        console.log('«Ура! нашел»')
+}
